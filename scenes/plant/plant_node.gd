2fe7a2aa4e9372261d08b91bdbf8901a9b692f2b
AJ Cloutier <ajcloutier@gmail.com>
2025-01-25 15:11:15 -0600

    remove left/right from plant_node

--- scenes/plant/plant_node.gd
+++ scenes/plant/plant_node.gd
@@ -9,43 +9,8 @@ const SPRITE_WIDTH = 11
 
 enum Direction {LEFT, RIGHT, UP}
 
-const PLANT_SIZE: Vector2 = Vector2i(SPRITE_WIDTH, SPRITE_HEIGHT)
-const DirectionMap: Dictionary = {
-	Direction.LEFT: {
-		"relative_position": Vector2i.LEFT * PLANT_SIZE.x,
-		"key": "left"
-	},
-	Direction.RIGHT: {
-		"relative_position": Vector2i.RIGHT * PLANT_SIZE.x,
-		"key": "right"
-	},
-	Direction.UP: {
-		"relative_position": Vector2i.UP * PLANT_SIZE.y,
-		"key": "up"
-	}
-}
-
-@export var _left: PlantNode = null
-@export var _right: PlantNode = null
-@export var _up: PlantNode = null
-
-var left: PlantNode:
-	get:
-		return _left
-	set(value):
-		_left = value
-
-var right: PlantNode:
-	get:
-		return _right
-	set(value):
-		_right = value
-
-var up: PlantNode:
-	get:
-		return _up
-	set(value):
-		_up = value
+const NODE_SIZE: Vector2 = Vector2i(SPRITE_WIDTH, SPRITE_HEIGHT)
+const relative_position = Vector2i.UP * NODE_SIZE.y
 
 
 func _ready():
@@ -54,26 +19,6 @@ func _ready():
 
 func _process(_delta):
 	pass
-
-
-func get_leaves() -> Array[PlantNode]:
-	var leaves: Array[PlantNode] = []
-
-	if _left != null:
-		leaves.append_array(_left.get_leaves())
-	if _up != null:
-		leaves.append_array(_up.get_leaves())
-	if _right != null:
-		leaves.append_array(_right.get_leaves())
-	
-	if (leaves.size() == 0):
-		leaves.append(self)
-		
-	return leaves
-
-
-func has_immediate_leaves() -> bool:
-	return _left != null or _up != null or _right != null
 	
 	
 func prune() -> void:
