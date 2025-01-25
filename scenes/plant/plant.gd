85a7b8fa0bb4fd72a4a2950a921a406b4bc1928b
AJ Cloutier <ajcloutier@gmail.com>
2025-01-25 12:06:50 -0600

    add some plant logic

--- scenes/plant/plant.gd
+++ scenes/plant/plant.gd
@@ -1,3 +1,47 @@
 extends Node2D
 
 class_name Plant
+
+const PLANT_NODE = preload("res://scenes/plant/plant_node.tscn")
+@onready var grow_timer: Timer = $GrowTimer
+
+var _root: PlantNode
+
+func _ready():
+	_root = PlantNode.new()
+	_root.position = Vector2(50, 0)
+	add_child(_root)
+
+
+func should_grow() -> bool:
+	return true
+	# return randi() % 2 == 0
+func grow() -> void:
+	var leaves = _root.get_leaves()
+	for leaf in leaves:
+		# var should_grow_left = should_grow()
+		# var should_grow_right = should_grow()
+		var should_grow_up = should_grow()
+		if should_grow_up:
+			add_node(leaf, [PlantNode.Direction.UP])
+
+
+func add_node(source: PlantNode, directions: Array[PlantNode.Direction]) -> void:
+	for dir in directions:
+		var new_node = PLANT_NODE.instantiate()
+		var dir_info = PlantNode.DirectionMap[dir]
+		new_node.position = source.position + dir_info["relative_position"]
+		source[dir_info.key] = new_node
+		if (dir == PlantNode.Direction.UP):
+			_root.add_child(new_node)
+		else:
+			source.add_child(new_node)
+	
+
+func _on_grow_timer_timeout() -> void:
+	grow()
+	print("root", _root)
+	print("root.leaves", _root.get_leaves())
+	
+func get_timer() -> Timer:
+	return grow_timer
--- scenes/plant/plant_node.gd
+++ scenes/plant/plant_node.gd
@@ -2,9 +2,27 @@ extends Area2D
 
 class_name PlantNode
 
-var _left: PlantNode = null
-var _right: PlantNode = null
-var _up: PlantNode = null
+enum Direction {LEFT, RIGHT, UP}
+
+const PLANT_SIZE: Vector2 = Vector2i(16, 16)
+const DirectionMap: Dictionary = {
+	Direction.LEFT: {
+		"relative_position": Vector2i.LEFT * PLANT_SIZE.x,
+		"key": "left"
+	},
+	Direction.RIGHT: {
+		"relative_position": Vector2i.RIGHT * PLANT_SIZE.x,
+		"key": "right"
+	},
+	Direction.UP: {
+		"relative_position": Vector2i.UP * PLANT_SIZE.y,
+		"key": "up"
+	}
+}
+
+@export var _left: PlantNode = null
+@export var _right: PlantNode = null
+@export var _up: PlantNode = null
 
 var left: PlantNode:
 	get:
@@ -26,9 +44,27 @@ var up: PlantNode:
 
 
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+
+
+func _process(_delta):
+	pass
+
+
+func get_leaves() -> Array[PlantNode]:
+	var leaves: Array[PlantNode] = []
+
+	if _left != null:
+		leaves.append_array(_left.get_leaves())
+	if _up != null:
+		leaves.append_array(_up.get_leaves())
+	if _right != null:
+		leaves.append_array(_right.get_leaves())
+	
+	if (leaves.size() == 0):
+		leaves.append(self)
+		
+	return leaves
+
+func has_immediate_leaves() -> bool:
+	return _left != null or _up != null or _right != null
