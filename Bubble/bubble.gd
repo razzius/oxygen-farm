6e3aca6675c66c474afc8ebf2b156c38baa691c8
Andrew Lubinus <andrew.lubinus@gmail.com>
2025-01-25 16:10:16 -0600

    Adding pop function

---
+++ Bubble/bubble.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@export var new_scene: PackedScene
+
+func replace_children():
+	# Remove all existing children
+	for child in get_children():
+		remove_child(child)
+		child.queue_free()
+	
+	# Instance and add the new scene
+	if new_scene:
+		var instance = new_scene.instantiate()
+		instance.set_global_position(Vector2(0,0))
+		add_child(instance)
+	else:
+		push_warning("No scene reference set for replacement")
+	
+func pop() -> void:
+	replace_children()
