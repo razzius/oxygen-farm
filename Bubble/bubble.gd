8b7b1a418ba05498de031dadbaf6b01df3859b40
AJ Cloutier <ajcloutier@gmail.com>
2025-01-25 18:24:09 -0600

    add bubble pop logic

--- Bubble/bubble.gd
+++ Bubble/bubble.gd
@@ -12,10 +12,15 @@ func replace_children():
 	# Instance and add the new scene
 	if new_scene:
 		var instance = new_scene.instantiate()
-		instance.set_global_position(Vector2(0,0))
+		instance.set_global_position(Vector2(0, 0))
 		add_child(instance)
 	else:
 		push_warning("No scene reference set for replacement")
 	
 func pop() -> void:
 	replace_children()
+
+
+func _on_area_exited(area: Node2D):
+	if area is PlantNode or area is Player:
+		pop()
