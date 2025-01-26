6b1547fb5b0fb63065bb1bde9c62bd7d8f5c80f5
ijames428 <ijames428@gmail.com>
2025-01-26 12:35:15 -0600

    Percent is red if below quota, and green if above

--- VFX/value_bar.gd
+++ VFX/value_bar.gd
@@ -8,3 +8,8 @@ func _process(_delta: float) -> void:
 	var ratio: float = GameStateManager.GetOxygenPercent();
 	oxy_ratio.material.set_shader_parameter("Ratio", ratio);
 	value_label.text = "%d%%" % int(ratio * 100)
+	
+	if GameStateManager.GetOxygenPercent() >= GameStateManager.Quota / 100.0:
+		value_label.add_theme_color_override("font_color", Color(0.0, 1.0, 0.0))
+	else:
+		value_label.add_theme_color_override("font_color", Color(1.0, 0.0, 0.0))
