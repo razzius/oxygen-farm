3709d353e920ad5e64c98c0724634e12e321446b
ijames428 <ijames428@gmail.com>
2025-01-25 21:43:53 -0600

    Leaf pop implementation

--- Managers/GameStateManager.gd
+++ Managers/GameStateManager.gd
@@ -7,7 +7,6 @@ const OXYGEN_MAX: float = 1000.0
 var OxygenLevel: float = 0.0
 var OxygenDeltaPerPlant: float = 0.01
 var OxygenVelocity: float = 0.0
-var OxygenQuota: float = 0.0
 var NodeScale: Vector2
 
 var PlayerIsRunning : bool = false
@@ -16,9 +15,17 @@ var JetPackIsActive : bool = false
 var OxygenConsumptionRateFromRunning : float = 0.25
 var OxygenConsumptionRateFromJetPack : float = 1.0
 
+var QuotaMin: float = 20.0
+var QuotaMax: float = 80.0
+var Quota: float = 0.0
+
+var rng : RandomNumberGenerator
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	rng = RandomNumberGenerator.new()
+
 	SignalManager.on_plant_grow.connect(OnPlantGrow)
 	SignalManager.on_plant_node_removed.connect(OnPlantNodeRemoved)
 	SignalManager.on_jetpack_usage_changed.connect(OnJetpackUsageChanged)
@@ -41,7 +48,7 @@ func _process(delta: float) -> void:
 func OnPlantGrow() -> void:
 	OxygenVelocity += OxygenDeltaPerPlant
 
-func OnPlantNodeRemoved() -> void:
+func OnPlantNodeRemoved(position : Vector2) -> void:
 	OxygenVelocity -= OxygenDeltaPerPlant
 
 
@@ -55,3 +62,7 @@ func OnJetpackUsageChanged(is_using : bool) -> void:
 
 func OnRunningUsageChanged(is_running : bool) -> void:
 	PlayerIsRunning = is_running
+
+
+func CalculateQuota() -> void:
+	Quota = rng.randf_range(QuotaMin, QuotaMax)
