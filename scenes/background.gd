d297b529163162432a42e8271e2504c98655fbb9
Cole Cecil <cole.cecil@gmail.com>
2025-01-27 22:36:27 -0600

    Prevent background blinking animation from getting disabled

--- scenes/background.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatedSprite2D
-
-
-func _ready() -> void:
-	animation = "solid"
-	await get_tree().create_timer(2.0).timeout
-	animation = "blink"
-	await get_tree().create_timer(45.0).timeout
-	animation = "solid"
